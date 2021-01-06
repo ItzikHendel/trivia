45f2b850f9fef46c68cfaf454f6aa44aa70e9403
ItzikHendel <itsiche@gmail.com>
2021-01-06 20:46:08 +0200

    Adding material-ui

--- pages/itzik.js
+++ pages/itzik.js
@@ -1,3 +1,11 @@
-export default function Itzik () {
-    return <div> Itzik </div>
+import Button from '@material-ui/core/Button'
+
+export default function Itzik() {
+    return <div>
+        <Button variant="contained">Default</Button>
+        <Button variant="contained" color="primary">Primary</Button>
+        <Button variant="contained" color="secondary">Secondary</Button>
+        <Button variant="contained" disabled>Disabled</Button>
+        <Button variant="contained" color="primary" href="#contained-buttons">Link</Button>
+    </div>
 }
