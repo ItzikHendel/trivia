e20a4b47ad66b95257351fb524c0bb56fc2569ed
ItzikHendel <itsiche@gmail.com>
2021-01-07 18:38:22 +0200

    Move quizData into index

--- Components/Quiz.js
+++ Components/Quiz.js
@@ -3,24 +3,16 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import { shuffle } from 'underscore';
 
-export default function Quiz() {
-    const quizData = {
-        question: "Who of the following authors did not receive the Nobel prize?",
-        answers: ["Leo Tolstoy", "Isaac Bashevis Singer", "Gabriel García Márquez", "Günter Grass"],
-        category: "Literature",
-        quote: "Count Lev Nikolayevich Tolstoy (9 September 1828 – 20 November 1910), usually referred to in English as Leo Tolstoy, was a Russian writer who is regarded as one of the greatest authors of all time. He received multiple nominations for Nobel Prize in Literature every year from 1902 to 1906, and nominations for Nobel Peace Prize in 1901, 1902 and 1910, and his miss of the prize is a major Nobel prize controversy.",
-        source: "Wikipedia",
-        link: "https://en.wikipedia.org/wiki/Leo_Tolstoy"
-    };
-
-    const isCorrect = (answer) => answer === quizData.answers[0]
+export default function Quiz({ quizData }) {
+    const { question, answers } = quizData;
+    const handleAnswerSelected = (answer) => console.log("isCorrect: ", answer === quizData.answers[0]);
 
     return (
         <Container>
             <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} >
                 <Box bgcolor="primary.main" color="primary.contrastText" p={2}>{quizData.question}</Box>
                 {shuffle(quizData.answers).map((answer) =>
-                    <Box key={answer} bgcolor="text.disabled" color="background.paper" m={2} p={2} onClick={() => console.log("isCorrect: " , isCorrect(answer))}>
+                    <Box key={answer} bgcolor="text.disabled" color="background.paper" m={2} p={2} onClick={() => handleAnswerSelected(answer)}>
                         {answer}
                     </Box>)}
             </Typography>
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,15 @@ import Quiz from '../Components/Quiz';
 
 export default function Home() {
 
-  const isServer = typeof window === 'undefined';
+  const isServer = typeof window === 'undefined';  //Temp - to avoid mismatch class name by material ui
+  const quizData = {
+    question: "Who of the following authors did not receive the Nobel prize?",
+    answers: ["Leo Tolstoy", "Isaac Bashevis Singer", "Gabriel García Márquez", "Günter Grass"],
+    category: "Literature",
+    quote: "Count Lev Nikolayevich Tolstoy (9 September 1828 – 20 November 1910), usually referred to in English as Leo Tolstoy, was a Russian writer who is regarded as one of the greatest authors of all time. He received multiple nominations for Nobel Prize in Literature every year from 1902 to 1906, and nominations for Nobel Peace Prize in 1901, 1902 and 1910, and his miss of the prize is a major Nobel prize controversy.",
+    source: "Wikipedia",
+    link: "https://en.wikipedia.org/wiki/Leo_Tolstoy"
+  };
 
   return (
     <div>
@@ -16,7 +24,7 @@ export default function Home() {
 
       </Head>
 
-      {isServer ? null :  <Quiz />} //Temp - to avoid mismatch class name by material ui
+      {isServer ? null : <Quiz quizData={quizData} />}
     </div>
   )
 }
