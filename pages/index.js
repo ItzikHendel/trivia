3182df50887dd9affd32d602899bc39277a958ea
ItzikHendel <itsiche@gmail.com>
2021-01-11 16:45:15 +0200

    Move quiz data to sparate file

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import quizData from '../public/quizData';
 import Head from 'next/head'
 //import styles from '../styles/Home.module.css'
 
@@ -6,14 +7,6 @@ import Quiz from '../Components/Quiz';
 export default function Home() {
 
   const isServer = typeof window === 'undefined';  //Temp - to avoid mismatch class name by material ui
-  const quizData = {
-    question: "Who of the following authors did not receive the Nobel prize?",
-    answers: ["Leo Tolstoy", "Isaac Bashevis Singer", "Gabriel García Márquez", "Günter Grass"],
-    category: "Literature",
-    quote: "Count Lev Nikolayevich Tolstoy (9 September 1828 – 20 November 1910), usually referred to in English as Leo Tolstoy, was a Russian writer who is regarded as one of the greatest authors of all time. He received multiple nominations for Nobel Prize in Literature every year from 1902 to 1906, and nominations for Nobel Peace Prize in 1901, 1902 and 1910, and his miss of the prize is a major Nobel prize controversy.",
-    source: "Wikipedia",
-    link: "https://en.wikipedia.org/wiki/Leo_Tolstoy"
-  };
 
   return (
     <div>
