d7ac4c9699de522bd028ce070439ae71fa187130
ItzikHendel <itsiche@gmail.com>
2021-01-13 16:20:44 +0200

    Disable select after answer selected

--- Components/Quiz.js
+++ Components/Quiz.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
 const Quiz = React.memo(({ question, correctAnswer, shuffledAnswers, handleAnswerSelected }) => {
+    const [isAnswerSelected, setIsAnswerSelected] = useState(false);
+
+    useEffect(() => setIsAnswerSelected(false), [question]);
+
     const answerRow = (answer) =>
-        <Box key={answer} bgcolor="text.disabled" color="background.paper" m={2} p={2} onClick={() => handleAnswerSelected(answer === correctAnswer)}>
+        <Box
+            key={answer}
+            bgcolor={isAnswerSelected ? "text.disabled" : "text.secondary"}
+            color="background.paper"
+            m={2}
+            p={2}
+            onClick={() => {
+                if (!isAnswerSelected) {
+                    setIsAnswerSelected(true);
+                    handleAnswerSelected(answer === correctAnswer);
+                }
+            }}>
             {answer}
         </Box>;
 
