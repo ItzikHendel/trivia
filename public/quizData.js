76e220ad8cad06527c473ce57ed8719c64c3244e
ItzikHendel <itsiche@gmail.com>
2021-01-13 18:21:05 +0200

    Add quiz data

--- public/quizData.js
+++ public/quizData.js
@@ -16,6 +16,14 @@ const quizDataArray = [
     link: "https://www.biography.com/athlete/dwyane-wade"
   },
   {
+    question: "Who of the following American citizens have lain in honor in the United States Capitol Rotunda?",
+    answers: ["Rosa Parks", "Ruth Bader Ginsburg", "Albert Einstein", "Martin Luther King"],
+    category: "History",
+    quote: "Four Americans have lain in honor, beginning in 1998. Rosa Parks was the first woman to lie in honor in the United States Capitol.",
+    source: "Wikipedia",
+    link: "https://en.wikipedia.org/wiki/Lying_in_state"
+  },
+  {
     question: "Which NBA team has the best record of wins during the regular season?",
     answers: ["Golden State Warriors", "Chicago Bulls", "Los Angeles Lakers", "Boston Celtics"],
     category: "Sport",
@@ -23,6 +31,51 @@ const quizDataArray = [
     source: "Wikipedia",
     link: "https://en.wikipedia.org/wiki/2015%E2%80%9316_Golden_State_Warriors_season"
   },
+  {
+    question: "Which president has nominated Ruth Bader Ginsburg to the Supreme Court of the United States?",
+    answers: ["Bill Clinton", "George H.W. Bush", "Barack Obama", "George W. Bush"],
+    category: "History",
+    quote: "Joan Ruth Bader Ginsburg (March 15, 1933 – September 18, 2020) was an American lawyer and jurist who served as an associate justice of the Supreme Court of the United States from 1993 until her death in September 2020. She was nominated by President Bill Clinton, replacing retiring justice Byron White, and at the time was generally viewed as a moderate consensus-builder.",
+    source: "Wikipedia",
+    link: "https://en.wikipedia.org/wiki/Ruth_Bader_Ginsburg"
+  },
+  {
+    question: "What is the world’s tallest building?",
+    answers: ["Burj Khalifa, Dubai", "Shanghai Tower, Shanghai", "Abraj Al-Bait Clock Tower, Mecca", "Lotte World Tower, Seoul"],
+    category: "Geography",
+    quote: "Burj Khalifa, Khalifa also spelled Khalīfah, mixed-use skyscraper in Dubai, U.A.E., that is the world’s tallest building, according to all three of the main criteria by which such buildings are judged.",
+    source: "Britannica",
+    link: "https://www.britannica.com/topic/Burj-Khalifa"
+  },
+  {
+    question: "Who was the first African American actress or actor ever to be honored with an Oscar?",
+    answers: ["Hattie McDaniel, Gone With the Wind", "Sidney Poitier, Lilies of the Field", "Denzel Washington, Glory", "Noah Cullen, The Defiant Ones"],
+    category: "Movies",
+    quote: "On February 29, 1940, Gone with the Wind is honored with eight Oscars by the American Academy of Motion Picture Arts and Sciences. An epic Southern romance set during the hard times of the Civil War, the movie swept the prestigious Best Picture, Director, Screenplay, Cinematography, Art Direction, Film Editing, and Actress categories. However, the most momentous award that night undoubtedly went to Hattie McDaniel for her portrayal of “Mammy,” a housemaid and former enslaved woman. McDaniel, who won the Best Supporting Actress Academy Award, was the first African American actress or actor ever to be honored with an Oscar.",
+    source: "History",
+    link: "https://www.history.com/this-day-in-history/mcdaniel-wins-oscar"
+  },
+  {
+    question: "Who of the following NBA coaches won 3 times in the NBA Coach of the Year Award?",
+    answers: ["Gregg Popovich", "Phil Jackson", "Mike D'Antoni", "Hubie Brown"],
+    category: "Sport",
+    quote: "Gregg Popovich, Don Nelson and Pat Riley have each won the award three times, while Hubie Brown, Mike Budenholzer, Mike D'Antoni, Bill Fitch, Cotton Fitzsimmons, and Gene Shue have each won it twice.",
+    source: "Wikipedia",
+    link: "https://en.wikipedia.org/wiki/NBA_Coach_of_the_Year_Award"
+  },
+
+
+  
+
+
+  
+
+
+ 
+
+  
+
+  
 
 
   
