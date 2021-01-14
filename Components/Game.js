4b3aa335f96c6367babd0145f7852aecd960ae31
ItzikHendel <itsiche@gmail.com>
2021-01-14 12:09:44 +0200

    Adding stepper

--- Components/Game.js
+++ Components/Game.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import Container from '@material-ui/core/Container';
+import MobileStepper from '@material-ui/core/MobileStepper';
+//import Button from '@material-ui/core/Button';
+//import Icon from '@material-ui/core/Icon';
+
 import { shuffle } from 'underscore';
 
 import Quiz from './Quiz';
@@ -18,14 +22,14 @@ export default function Game({ quizDataArray, goToSummary }) {
         shuffledAnswers = shuffle(answers);
     }
 
-    const onNextClick = () => {        
+    const onNextClick = () => {
         setIsAnswerCorrect(null);
         setQuizIndex(quizIndex + 1);
         shuffledAnswers = null;
     };
 
     let actionButton;
-    
+
     if (isAnswerCorrect === null) {
         actionButton = null;
     } else if (quizIndex + 1 < quizDataArray.length) {
@@ -33,12 +37,37 @@ export default function Game({ quizDataArray, goToSummary }) {
     } else {
         actionButton = <ActionButton text="Summary" color="secondary" icon="list_alt" onClick={() => console.log("==== Summary clicked! ")} />
     }
-    
+
+    /*const stepperNextButton =
+        <Button size="small" onClick={onNextClick} disabled={isAnswerCorrect === null || quizIndex + 1 === quizDataArray.length}>
+            Next <Icon>{"keyboard_arrow_right"}</Icon>
+        </Button>;*/
+
     return (
         <Container>
-            <Quiz question={question} correctAnswer={answers[0]} shuffledAnswers={shuffledAnswers} handleAnswerSelected={setIsAnswerCorrect} />
-            <Result isAnswerCorrect={isAnswerCorrect} correctAnswer={answers[0]} quote={quote} source={source} link={link} />
-            { actionButton }
+            <Quiz
+                question={question}
+                correctAnswer={answers[0]}
+                shuffledAnswers={shuffledAnswers}
+                handleAnswerSelected={setIsAnswerCorrect} />
+
+            <MobileStepper
+                variant="dots"
+                steps={quizDataArray.length}
+                position="static"
+                activeStep={quizIndex}
+                style={{ justifyContent: 'center' }}
+            //nextButton={stepperNextButton}
+            />
+
+            <Result
+                isAnswerCorrect={isAnswerCorrect}
+                correctAnswer={answers[0]} quote={quote}
+                source={source}
+                link={link} />
+
+            {actionButton}
+
         </Container>
     );
 };
