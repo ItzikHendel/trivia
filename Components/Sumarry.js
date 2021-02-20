d7115463455f6fbd5455b1ccb862d56c99fc364c
ItzikHendel <itsiche@gmail.com>
2021-02-20 09:16:34 +0200

    Show summary real result number

--- Components/Sumarry.js
+++ Components/Sumarry.js
@@ -5,11 +5,14 @@ import Box from '@material-ui/core/Box';
 export default function Summary({ summaryData }) {
     console.log("=========");
     console.log(summaryData);
+
+    const correctAnswersNum = summaryData.reduce((total, current) =>  current.isCorrect ? total + 1 : total, 0);
+
     return (
         <Container>
-            <Typography variant="h1">          
-                <Box display="flex" justifyContent="center">                   
-                    <Box component="span" color="white" bgcolor="palevioletred" borderRadius={60} p={1}>3/10</Box>
+            <Typography variant="h1">
+                <Box display="flex" justifyContent="center">
+                    <Box component="span" color="white" bgcolor="palevioletred" borderRadius={60} p={1} m={5}>{correctAnswersNum}/{summaryData.length}</Box>
                 </Box>
             </Typography>
         </Container>
